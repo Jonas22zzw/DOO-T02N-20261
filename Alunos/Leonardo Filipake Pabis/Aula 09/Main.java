10cd438434029b1b79f339eb6f2ba48165fa3293
leofpabis <leofilipake@gmail.com>
2026-05-06 20:32:56 -0300

    começar

---
+++ Alunos/Leonardo Filipake Pabis/Aula 09/Main.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculadora");
+        frame.setSize(400, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.white);
+
+        JButton 
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
